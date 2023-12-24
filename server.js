f7ed35742662ff518f6aedb6014b99d4cbb1e9fc
cuber-dev <118879829+cuber-dev@users.noreply.github.com>
2023-12-24 08:39:50 +0000

    rtr

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const app = require("express")();
+const path = require("path");
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.json({
     msg: "server is running!",
   });
+});
+app.get("/apk", (req, res) => {
+  const apkFile = path.resolve(__dirname + "/apks/andorid-test-1.apk");
+  res.sendFile(apkFile);
 });
 
 app.listen(port, () => {
